fe908a6b0fbfbcd8f49910e4591a51b2f71af0ef
Varun Veerbhadra <54136118+varunveeraa@users.noreply.github.com>
2021-01-02 20:18:23 +0530

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -1,4 +1,4 @@
-#import module from tkinter for UI
+#importing libraries and module
 from tkinter import *
 import os
 from datetime import datetime;
@@ -8,8 +8,9 @@ root=Tk()
 
 root.configure(background="black")
 
-#root.geometry("300x300")
+root.geometry("350x350")
 
+#function creation
 def function1():
     
     os.system("face_detection_1.py")
@@ -26,11 +27,11 @@ def function4():
 
     root.destroy()
 
-#stting title for the window
+#naming the title of the window
 root.title("Detections")
 
 #creating a text label
-Label(root, text="Detections Using Haar Cascades",font=("times new roman",20),fg="white",bg="black",height=2).grid(row=0,rowspan=2,columnspan=2,sticky=N+E+W+S,padx=5,pady=5)
+Label(root, text="Face related detections",font=("times new roman",20),fg="white",bg="black",height=2).grid(row=0,rowspan=2,columnspan=2,sticky=N+E+W+S,padx=5,pady=5)
 
 #initiating first button
 Button(root,text="Face Detection",font=("times new roman",20),bg="#000000",fg='green',command=function1).grid(row=2,columnspan=2,sticky=W+E+N+S,padx=5,pady=5)
@@ -45,4 +46,3 @@ Button(root,text="Smile Detection",font=('times new roman',20),bg="#000000",fg="
 Button(root,text="Quit",font=('times new roman',20),bg="black",fg="red",command=function4).grid(row=5,columnspan=2,sticky=N+E+W+S,padx=5,pady=5)
 
 root.mainloop()
-
